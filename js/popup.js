a8785d3fe11b69eeb3624f11a96c655c749ddac9
binnng <imbinnng@gmail.com>
2013-03-21 12:38:36 +0800

    增加了短网址服务

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,15 @@
-+function( document ) {
++function( document, $ ) {
 
-	var qrcode = document.getElementById('qrcode');
+	var qrcode = document.getElementById('qrcode'),
+		url = document.getElementById('url');
 
 	chrome.tabs.getSelected(function( tab ){
 		qrcode.src = 'http://chart.apis.google.com/chart?chs=400x400&cht=qr&chl=' + tab.url;
+
+		$.getJSON('http://126.am/api!shorten.action?key=2559c3c1da424bf89c4151627c072ff1&longUrl=' + tab.url, function( data ) {
+			url.innerHTML = data.url;
+		});
+		
 	})
 
-}( document );
+}( document, jQuery );
