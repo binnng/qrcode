2adfe3d958fd068b85e69363c6c2c25391419bc1
binnng <imbinnng@gmail.com>
2013-04-18 13:34:40 +0800

    改用本地绘制二维码

--- js/popup.js
+++ js/popup.js
@@ -4,7 +4,9 @@
 		url = document.getElementById('url');
 
 	chrome.tabs.getSelected(function( tab ){
-		qrcode.src = 'http://chart.apis.google.com/chart?chs=400x400&cht=qr&chl=' + tab.url;
+		/*qrcode.src = 'http://chart.apis.google.com/chart?chs=400x400&cht=qr&chl=' + tab.url;*/
+
+		$(qrcode).qrcode({width: 300,height: 300,text: tab.url});
 
 		$.getJSON('http://126.am/api!shorten.action?key=2559c3c1da424bf89c4151627c072ff1&longUrl=' + tab.url, function( data ) {
 			url.innerHTML = data.url;
